8f522c77bc50b7229ed3fe1f73db35a326601bfc
Dok6857 <141351808+Dok6857@users.noreply.github.com>
2023-12-16 12:06:17 +0200

    start

--- src/index.js
+++ src/index.js
@@ -14,3 +14,30 @@
  * прибрати завдання зі списку.
  * Список із завданнями має бути доступним після перезавантаження сторінки.
  */
+const form = document.querySelector('#task-form');
+const taskList = document.getElementById('task-list');
+
+form.addEventListener('submit', submitHandler);
+
+function submitHandler(event) {
+  event.preventDefault();
+
+  // const { taskName, taskText } = event.target.elements;
+  const taskNameValue = event.target.elements.taskName.value;
+  const taskTextValue = event.target.elements.taskText.value;
+
+  taskList.insertAdjacentHTML(
+    'afterbegin',
+    createMarkupItem(taskNameValue, taskTextValue)
+  );
+}
+
+function createMarkupItem(title, description) {
+  return `
+    <li class="task-list-item">
+        <button class="task-list-item-btn">Удалить</button>
+        <h3>${title}</h3>
+        <p>${description}</p>
+    </li>
+    `;
+}
